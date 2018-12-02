ba531088f81b25ca938bceab0f9b9a9e5d373bae
Krissy Dukes <krissydotnet@yahoo.com>
2018-12-02 11:14:39 -0500

    Updates

---
+++ precache-manifest.0ab30567981ddee6696c949f42f7c482.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fd8609f0db8252f3896",
+    "url": "/ericascupcakery/static/js/runtime~main.3fd8609f.js"
+  },
+  {
+    "revision": "b32b69cce77f7debfd59",
+    "url": "/ericascupcakery/static/js/main.b32b69cc.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/ericascupcakery/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "b32b69cce77f7debfd59",
+    "url": "/ericascupcakery/static/css/main.59fc0776.chunk.css"
+  },
+  {
+    "revision": "41e573f9c6b3ffb204240643a27c4604",
+    "url": "/ericascupcakery/index.html"
+  }
+];
